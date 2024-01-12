6dfa46f61ae5f6c84a96189aea938953c62f55df
cypsyco <jyh0872@gmail.com>
2024-01-12 15:45:29 +0900

    first commit

---
+++ src/test/java/com/example/atmospeer/AtmospeerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.atmospeer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtmospeerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
